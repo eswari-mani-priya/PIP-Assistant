34846aee176b7d69e181bef7082343cdee42724f
eswari-mani-priya <eswari.gangikunta@gmail.com>
2019-09-27 16:11:33 +0530

    Added dialogflow api to get data from dialogflow

---
+++ pip_assistant/pip_bot/helper_functions.py
@@ -0,0 +1,27 @@
+# __author__ == "Priya"
+import dialogflow_v2 as dflow
+import uuid
+import os
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.path.join(os.getenv('HOME'), 'PIP-ASSISTANT.json')
+
+texts=['hi','python']
+session_client = dflow.SessionsClient()
+session_id = uuid.uuid4()
+project_id='pip-assistant'
+session = session_client.session_path(project_id, session_id)
+print('Session path: {}\n'.format(session))
+
+for text in texts:
+    text_input = dflow.types.TextInput(text=text, language_code='en')
+    query_input = dflow.types.QueryInput(text=text_input)
+    response = session_client.detect_intent(session=session, query_input=query_input)
+    print('Query text: {}'.format(response.query_result.query_text))
+    print('Detected intent: {} (confidence: {})\n'.format(
+        response.query_result.intent.display_name,
+        response.query_result.intent_detection_confidence))
+    print('Fulfillment text: {}\n'.format(
+        response.query_result.fulfillment_text))
+
+
+
