ecf8474928e987590129cf3e1055dcee889594f9
eswari-mani-priya <eswari.gangikunta@gmail.com>
2019-09-28 22:42:55 +0530

    Added a rough html for chatbox

--- pip_assistant/pip_bot/helper_functions.py
+++ pip_assistant/pip_bot/helper_functions.py
@@ -5,14 +5,12 @@ import os
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.path.join(os.getenv('HOME'), 'PIP-ASSISTANT.json')
 
-texts=['hi','python']
-session_client = dflow.SessionsClient()
-session_id = uuid.uuid4()
-project_id='pip-assistant'
-session = session_client.session_path(project_id, session_id)
-print('Session path: {}\n'.format(session))
-
-for text in texts:
+def get_response(text):
+    session_client = dflow.SessionsClient()
+    session_id = uuid.uuid4()
+    project_id='pip-assistant'
+    session = session_client.session_path(project_id, session_id)
+    print('Session path: {}\n'.format(session))
     text_input = dflow.types.TextInput(text=text, language_code='en')
     query_input = dflow.types.QueryInput(text=text_input)
     response = session_client.detect_intent(session=session, query_input=query_input)
@@ -22,6 +20,9 @@ for text in texts:
         response.query_result.intent_detection_confidence))
     print('Fulfillment text: {}\n'.format(
         response.query_result.fulfillment_text))
+    res = response.query_result.fulfillment_text
+    return res
+
 
 
 
